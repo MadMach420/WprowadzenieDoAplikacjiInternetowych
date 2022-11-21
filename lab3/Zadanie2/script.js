14000cde37e3769d613a25fe1a4d51695a668b93
MadMach420 <mach.krzsztf@gmail.com>
2022-11-21 15:49:15 +0100

    lab3 gotowe

--- lab3/Zadanie2/script.js
+++
@@ -1,9 +0,0 @@
-const photos = ["./images/img1", "./images/img2", "./images/img3"]
-let currIndex = 1
-
-function foo() {
-    document.getElementById("photo").src = photos[currIndex]
-    currIndex = (currIndex + 1) % photos.length
-}
-
-document.getElementById("change-photo").addEventListener("click", foo)
